7ae38e1050f5580cedc2e8246a2f81f0f1d4241f
davecamp7744 <92772510+davecamp7744@users.noreply.github.com>
2023-05-15 18:12:13 -0600

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,37 @@
+//FUNCION PARA EL TEXTO
+function send_message(message) {
+$.ajax({
+    type: 'POST',
+    url: '/message',
+    data: { 'message': message },
+    success: function (response) {
+    $('#chat').append('<div>' + response + '</div>');
+    }
+});
+}
+
+
+//OCULTAR BUTTON
+const button = document.querySelector('#mi-boton');
+button.addEventListener('click', () => {
+    button.style.display = 'none';
+});
+const btnenvi = document.querySelector('#btn-enviar');
+btnenvi.addEventListener('click', () => {
+    btnenvi.style.display = 'none';
+})
+//FUNCION PARA EL NOMBRE
+function send() {
+    var userinput = document.getElementById("userinput").value;
+    document.getElementById("chatbox").innerHTML += "<p><strong>Tú:</strong> " + userinput + "</p>";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("chatbox").innerHTML += "<p><strong>Iztak:</strong> " + this.responseText + "</p>";
+        }
+    };
+    xhttp.open("GET", "/get?msg=" + userinput, true);
+    xhttp.send();
+    document.getElementById("userinput").value = "";
+};
 
